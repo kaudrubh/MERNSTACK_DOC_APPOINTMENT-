491994d1b6ad4441c0d863e62189ce4d59ca720a
kaudrubh <139002590+kaudrubh@users.noreply.github.com>
2024-12-23 15:19:36 +0530

    Update AppContext.jsx

--- prescripto-full-stack/admin/src/context/AppContext.jsx
+++ prescripto-full-stack/admin/src/context/AppContext.jsx
@@ -6,8 +6,7 @@ export const AppContext = createContext()
 const AppContextProvider = (props) => {
 
     const currency = import.meta.env.VITE_CURRENCY
-    const backendUrl = import.meta.env.VITE_BACKEND_URL
-
+    const backendUrl = "https://b9gwnx4ypifgj5efwq2cvx-backend.onrender.com"
     const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
 
     // Function to format the date eg. ( 20_01_2000 => 20 Jan 2000 )
