b2b41d855673a0c698ac4f6c9d9a5c71fa026388
kaudrubh <139002590+kaudrubh@users.noreply.github.com>
2024-12-23 19:22:40 +0530

    Update About2.jsx

--- prescripto-full-stack/frontend/src/pages/About.jsx
+++ prescripto-full-stack/frontend/src/pages/About.jsx
@@ -15,7 +15,7 @@ const About = () => {
           <p>Welcome to MediHub, your reliable partner in simplifying and managing your healthcare needs with ease and efficiency. At MediHub, we recognize the difficulties people often encounter when scheduling doctor appointments and organizing their medical records.</p>
           <p>MediHub is dedicated to innovation in healthcare technology. We are constantly working to enhance our platform, leveraging cutting-edge advancements to provide an exceptional user experience and outstanding service. Whether you're booking your first consultation or overseeing long-term care, MediHub is here to guide and support you every step of the way.</p>
           <b className='text-gray-800'>Our Vision</b>
-          <p>Our vision at Prescripto is to create a seamless healthcare experience for every user. We aim to bridge the gap between patients and healthcare providers, making it easier for you to access the care you need, when you need it.</p>
+          <p>Our vision at Medihub is to create a seamless healthcare experience for every user. We aim to bridge the gap between patients and healthcare providers, making it easier for you to access the care you need, when you need it.</p>
         </div>
       </div>
 
